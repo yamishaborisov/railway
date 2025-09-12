1556983c41f74bf5a57300d2128dfe0ef7134811
yamishaborisov <borisov3676@gmail.com>
2025-09-12 20:51:01 +0300

    feat(1) почистил всякий кал

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,7 @@
-import { Button } from 'primereact/button';
-
 export default function App() {
-	return (
-		<div className='app'>
-			<h1>My App</h1>
-			<Button label='Prime Button' />
-		</div>
-	);
+    return (
+        <div className="app">
+            <h1>My App</h1>
+        </div>
+    )
 }
