44b8c3be42c54e1dd328046cecc0ffb8065c0b76
yamishaborisov <borisov3676@gmail.com>
2025-11-02 19:38:25 +0300

    feat(11)/header with nav only for desktop

---
+++ src/shared/lib/config/routes.ts
@@ -0,0 +1,6 @@
+export const HEADER_NAV = [
+	{ name: 'Mobile App', link: '#' },
+	{ name: 'FAQs', link: '#' },
+	{ name: 'Contact', link: '#' },
+	{ name: 'Sign Up', link: '#', cta: true },
+];
---
+++ src/widgets/header/index.tsx
@@ -0,0 +1,48 @@
+import styles from './styles.module.scss';
+import clsx from 'clsx';
+import { HEADER_NAV } from '@/shared/lib/config/routes';
+
+type HeaderProps = {
+	className?: string;
+	bg?: 'light' | 'dark';
+};
+
+export const Header = ({ className, bg = 'light', ...props }: HeaderProps) => {
+	const isDark = bg === 'dark';
+
+	const img = (name: string) => `/images/${name}${isDark ? '-white' : ''}.svg`;
+
+	return (
+		<header className={clsx(styles.header, styles[bg], className)}>
+			<div className={styles.inner}>
+				<a className={styles.headerBrand} href='#'>
+					<img src={img('railway')} className={styles.headerLogo} />
+					<span className={styles.headerBrandName}>Railway</span>
+				</a>
+
+				<nav className={styles.headerNav}>
+					<ul className={styles.headerNavList}>
+						{HEADER_NAV.map(item => (
+							<li key={item.name} className={styles.headerNavItem}>
+								<a
+									href={item.link}
+									className={clsx(styles.link, item.cta && styles.cta)}
+								>
+									{item.name}
+								</a>
+							</li>
+						))}
+					</ul>
+				</nav>
+				<button
+					type='button'
+					className={clsx(
+						styles.headerBurger,
+						isDark ? styles.headerBurgerDark : styles.headerBurgerLight
+					)}
+					aria-label='Open menu'
+				></button>
+			</div>
+		</header>
+	);
+};
